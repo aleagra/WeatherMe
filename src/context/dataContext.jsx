c63fcc18a5f0d4efaa9eee9cba17ac76efd5c7a4
Lucas Samuel Pereyra <lucaspereyra470@gmail.com>
2023-03-16 17:10:04 -0300

    first styles added

---
+++ src/context/dataContext.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function dataContext() {
+    return <div>dataContext</div>;
+}
+
+export default dataContext;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,13 @@
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            color: {
+                black: '#1d1d1d',
+                white: '#ffffff',
+                primary: '#8A2DE0',
+            },
+        },
+    },
+    plugins: [],
+};
