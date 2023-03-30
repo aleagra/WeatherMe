892e33f58d6d3d7eb3c69f9531207b005f803097
Lucas Samuel Pereyra <lucaspereyra470@gmail.com>
2023-03-30 14:09:30 -0300

    imageloader finished

--- src/components/Home/Weather.jsx
+++ src/components/Home/Weather.jsx
@@ -1,26 +1,21 @@
 import React from 'react';
 import { DropletIcon, SunIcon, WindIcon } from '../../utilities';
+import ImageLoader from './ImageLoader';
 
 function Weather({ data }) {
     return (
         <>
             {data && (
-                <div className="text-white flex flex-col text-center gap-[2rem] skew-x-[10deg]">
-                    <div className="flex flex-col gap-[12rem] justify-center items-center">
+                <div className="text-white flex flex-col text-center gap-[2rem] skew-x-[10deg] w-[50%]">
+                    <div className="flex flex-col gap-[25rem] justify-center items-center">
                         <span className="relative text-4xl ml-auto z-[10] opacity-30 top-[2rem]">
                             <span className="text-8xl">{parseInt(data.main.temp)}</span>°C
                         </span>
-                        <div className="relative top-[-18rem] w-full z-[9]">
-                            <img
-                                src="./26.png"
-                                alt=""
-                                className="absolute w-full object-contain h-[30rem]"
-                            />
-                        </div>
-                        <span className="text-7xl capitalize font-bold">
+                        <span className="text-7xl capitalize font-bold whitespace-nowrap">
                             {data.weather[0].description}
                         </span>
                     </div>
+                    <ImageLoader />
                     <hr className="border-white/20" />
                     <div className="flex text-left gap-[2rem] justify-center">
                         <div className="flex gap-3 justify-start">
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,5 +1,6 @@
 export { default as Cards } from './Cards';
 export { default as Weather } from './Home/Weather';
+export { default as ImageLoader } from './Home/ImageLoader';
 export { default as WeekWeather } from './Home/WeekWeather';
 export { default as Navbar } from './Navbar/Navbar';
 export { default as WeatherContext } from './context/WeatherContext';
