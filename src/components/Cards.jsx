ac895be1f00e8801e8f79c0022a2b3b60e8dc545
Alejandro Agra <aleagra08@hotmail.com>
2023-04-12 19:58:21 -0300

    week weather

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,143 +1,211 @@
-import { useState } from 'react';
-import ImageLoader from './Home/ImageLoader';
-import { Splide, SplideSlide, SplideTrack } from '@splidejs/react-splide';
-import '@splidejs/react-splide/css';
+import { useState } from "react";
+import ImageLoader from "./Home/ImageLoader";
+import { Splide, SplideSlide, SplideTrack } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
 
 // or other themes
-import '@splidejs/react-splide/css/skyblue';
-import '@splidejs/react-splide/css/sea-green';
+import "@splidejs/react-splide/css/skyblue";
+import "@splidejs/react-splide/css/sea-green";
 
 // or only core styles
-import '@splidejs/react-splide/css/core';
+import "@splidejs/react-splide/css/core";
+import { DropletIcon, SunIcon, WindIcon } from "../utilities";
 
 const Cards = ({ data, dates }) => {
-    const [active, setActive] = useState(false);
-    const [details, setDetails] = useState([]);
+  const [active, setActive] = useState(false);
+  const [details, setDetails] = useState([]);
 
-    return (
-        <>
-            <Splide
-                hasTrack={false}
-                options={{
-                    perPage: 4,
-                    rewind: false,
-                    perMove: 1,
-                    gap: '3rem',
-                    padding: '1.5rem',
-                    pagination: false,
-                }}
-            >
-                <SplideTrack>
-                    {dates.map((date, index) => {
-                        const items = data[date].map((item) => item); // Guarda los items en un nuevo array
-                        const itemsZero = items[0];
-                        return (
-                            <SplideSlide key={index}>
-                                <div
-                                    className="px-[1rem] h-[25rem] py-4 rounded-2xl bg-blue-500 shadow-md  text-white hover:bg-blue-700/90 transition-colors cursor-pointer"
-                                    onClick={() => {
-                                        setActive(true);
-                                        setDetails(items);
-                                    }}
-                                >
-                                    <div className="flex gap-1 flex-col text-center h-full w-full">
-                                        <span>{date}</span>
-                                        <ImageLoader
-                                            data={itemsZero}
-                                            divClassName="mt-6"
-                                            imgClassName="w-full h-[11rem]"
-                                        />
-                                        <div className="flex flex-col mt-auto mx-auto">
-                                            <span className="mb-6 text-5xl font-bold">
-                                                {parseInt(itemsZero.main.temp)} °C
-                                            </span>
-                                            <div className="flex gap-x-4">
-                                                <div className="flex flex-col gap-y-1">
-                                                    <span className="text-sm opacity-80">
-                                                        Humidity
-                                                    </span>
-                                                    <span className="text-lg font-bold">
-                                                        {itemsZero.main.humidity}{' '}
-                                                        <span className="text-sm font-light">
-                                                            %
-                                                        </span>
-                                                    </span>
-                                                </div>
-                                                <div className="flex flex-col gap-y-1">
-                                                    <span className="text-sm opacity-80">
-                                                        Feels like
-                                                    </span>
-                                                    <span className="text-lg font-bold">
-                                                        {parseInt(itemsZero.main.feels_like)}{' '}
-                                                        <span className="text-sm font-light">
-                                                            °C
-                                                        </span>
-                                                    </span>
-                                                </div>
-                                                <div className="flex flex-col gap-y-1">
-                                                    <span className="text-sm opacity-80">Wind</span>
-                                                    <span className="text-lg font-bold">
-                                                        {itemsZero.wind.speed}{' '}
-                                                        <span className="text-sm font-light">
-                                                            km/h
-                                                        </span>
-                                                    </span>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </SplideSlide>
-                        );
-                    })}
-                </SplideTrack>
-                <div className="splide__arrows">
-                    <button className="splide__arrow splide__arrow--prev">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            strokeWidth={1.5}
-                            stroke="currentColor"
-                            className="w-6 h-6 stroke-blue-700"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M8.25 4.5l7.5 7.5-7.5 7.5"
-                            />
-                        </svg>
-                    </button>
-                    <button className="splide__arrow splide__arrow--next">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            strokeWidth={1.5}
-                            stroke="currentColor"
-                            className="w-6 h-6 stroke-blue-700"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M8.25 4.5l7.5 7.5-7.5 7.5"
-                            />
-                        </svg>
-                    </button>
-                </div>
-            </Splide>
+  const [selectedCardIndex, setSelectedCardIndex] = useState(null);
 
-            {active && (
-                <div className="px-[5rem]">
-                    <div className="w-full h-fit bg-black text-white">
-                        {details.map((item) => (
-                            <p>{item.main.temp}</p>
-                        ))}
+  const handleCardClick = (index) => {
+    setSelectedCardIndex(index);
+  };
+
+  const getDayOfWeek = (dateString) => {
+    const daysOfWeek = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    const dateParts = dateString.split("-");
+    const year = parseInt(dateParts[0]);
+    const month = parseInt(dateParts[1]) - 1;
+    const day = parseInt(dateParts[2]);
+    const dateObject = new Date(year, month, day);
+    const dayOfWeek = dateObject.getDay();
+    return daysOfWeek[dayOfWeek];
+  };
+
+  return (
+    <>
+      <Splide
+        hasTrack={false}
+        options={{
+          perPage: 4,
+          rewind: false,
+          perMove: 1,
+          gap: "3rem",
+          padding: "1.5rem",
+          pagination: false,
+        }}
+      >
+        <SplideTrack>
+          {dates.map((date, index) => {
+            const items = data[date].map((item) => item); // Guarda los items en un nuevo array
+            const itemsZero = items[0];
+            return (
+              <SplideSlide key={index}>
+                <div
+                  className={`px-[1rem] h-[27rem] py-4 rounded-2xl bg-blue-500 shadow-md  text-white transition-colors cursor-pointer ${
+                    selectedCardIndex === index ? "bg-blue-700/90" : "" // Cambia el color de fondo si la card está seleccionada
+                  }`}
+                  onClick={() => {
+                    setActive(true);
+                    setDetails(items);
+                    handleCardClick(index);
+                  }}
+                >
+                  <div className="flex gap-1 flex-col text-center h-full w-full">
+                    <span>{getDayOfWeek(date)}</span>
+                    <span>{date}</span>
+                    <div className="flex items-center flex-col ">
+                      <ImageLoader
+                        data={itemsZero}
+                        divClassName="mt-6"
+                        imgClassName="w-full h-[11rem] "
+                      />
                     </div>
+                    <div className="flex flex-col mt-auto mx-auto">
+                      <span className="mb-6 text-5xl font-bold">
+                        {parseInt(itemsZero.main.temp)} °C
+                      </span>
+                      <div className="flex gap-x-4">
+                        <div className="flex flex-col gap-y-1">
+                          <span className="text-sm opacity-80">Humidity</span>
+                          <span className="text-lg font-bold">
+                            {itemsZero.main.humidity}{" "}
+                            <span className="text-sm font-light">%</span>
+                          </span>
+                        </div>
+                        <div className="flex flex-col gap-y-1">
+                          <span className="text-sm opacity-80">Feels like</span>
+                          <span className="text-lg font-bold">
+                            {parseInt(itemsZero.main.feels_like)}{" "}
+                            <span className="text-sm font-light">°C</span>
+                          </span>
+                        </div>
+                        <div className="flex flex-col gap-y-1">
+                          <span className="text-sm opacity-80">Wind</span>
+                          <span className="text-lg font-bold">
+                            {itemsZero.wind.speed}{" "}
+                            <span className="text-sm font-light">km/h</span>
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-            )}
-        </>
-    );
+              </SplideSlide>
+            );
+          })}
+        </SplideTrack>
+        <div className="splide__arrows">
+          <button className="splide__arrow splide__arrow--prev">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6 stroke-blue-700"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M8.25 4.5l7.5 7.5-7.5 7.5"
+              />
+            </svg>
+          </button>
+          <button className="splide__arrow splide__arrow--next">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6 stroke-blue-700"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M8.25 4.5l7.5 7.5-7.5 7.5"
+              />
+            </svg>
+          </button>
+        </div>
+      </Splide>
+
+      {active && (
+        <div className="px-[4rem]">
+          <div className="w-full h-fit bg-blue-500 rounded-lg text-white items-center">
+            {details.map((item) => (
+              <div className="w-full flex gap-10 p-[2rem] items-center">
+                <div className="flex w-full justify-center">
+                  <span className="text-xl">
+                    {item.dt_txt.split(" ")[1].slice(0, 5)} HS
+                  </span>
+                </div>
+                <div className="flex w-full items-center">
+                  <ImageLoader data={item} imgClassName="h-[5rem]" />
+                  <p className="uppercase w-full">
+                    {item.weather[0].description}
+                  </p>
+                </div>
+
+                {/* <div className="flex w-full justify-center">
+                  <p className="uppercase">
+                    {" "}
+                    {parseInt(item.main.temp)} °C {console.log(item)}{" "}
+                  </p>
+                </div> */}
+                {/* <div className="flex w-full justify-center ">
+                  <p className="uppercase">{parseInt(item.main.feels_like)}</p>
+                </div> */}
+                <div className="flex w-full gap-3 justify-center">
+                  <SunIcon />
+                  <div className="flex  flex-col">
+                    <span>{parseInt(item.main.temp)} °C</span>
+                  </div>
+                </div>
+                <div className="flex gap-3 justify-center">
+                  <DropletIcon />
+                  <div className="flex flex-col  ">
+                    <span>{item.main.humidity}%</span>
+                  </div>
+                </div>
+                <div className="flex w-full gap-3 justify-center">
+                  <WindIcon />
+                  <div className="flex flex-col ">
+                    <span className="">{item.wind.speed} Km/h</span>
+                  </div>
+                </div>
+                <div className="flex w-full gap-3 justify-center">
+                  <SunIcon />
+                  <div className="flex  flex-col">
+                    <span>{parseInt(item.main.feels_like)} °C</span>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Cards;
--- src/components/Home/ImageLoader.jsx
+++ src/components/Home/ImageLoader.jsx
@@ -1,86 +1,87 @@
-import React, { useContext, useState } from 'react';
-import { WeatherContext } from '../context/WeatherContext';
+import React, { useContext, useState } from "react";
+import { WeatherContext } from "../context/WeatherContext";
 
 const ImageLoader = ({ data, divClassName, imgClassName }) => {
-    let weather = data.weather[0].description;
-    const [loading, setLoading] = useState(true);
-    let src;
+  let weather = data.weather[0].description;
+  const [loading, setLoading] = useState(true);
+  let src;
 
-    switch (weather) {
-        case 'clear sky':
-            src = './sun.png';
-            break;
-        case 'few clouds':
-        case 'scattered clouds':
-            src = './sun-cloud.png';
-            break;
-        case 'broken clouds':
-            src = './broken-clouds.png';
-            break;
-        case 'overcast clouds':
-        case 'cloud':
-        case 'smoke':
-        case 'mist':
-        case 'haze':
-        case 'hurricane':
-        case 'tornado':
-            src = './cloud.png';
-            break;
-        case 'light rain':
-            src = './rain-sun.png';
-            break;
-        case 'moderate rain':
-        case 'heavy rain':
-        case 'intense rain':
-            src = './rain.png';
-            break;
-        case 'light shower rain':
-        case 'moderate shower rain':
-        case 'heavy shower rain':
-            src = './shower-rain.png';
-            break;
-        case 'light snow':
-        case 'moderate snow':
-        case 'heavy snow':
-            src = './winter.png';
-            break;
-        case 'thunderstorm':
-        case 'heavy thunderstorm':
-            src = './thunder.png';
-            break;
-        case 'rain and thunder':
-            src = './rain-thunder.png';
-            break;
-        case 'snow and thunder':
-            src = './snow-thunder.png';
-            break;
-        case 'sandstorm':
-        case 'duststorm':
-            src = './tormenta.png';
-            break;
-        default:
-            src = './cloud.png';
-            break;
-    }
+  switch (weather) {
+    case "clear sky":
+      src = "./sun.png";
+      break;
+    case "few clouds":
+    case "scattered clouds":
+      src = "./sun-cloud.png";
+      break;
+    case "broken clouds":
+      src = "./broken-clouds.png";
+      break;
+    case "overcast clouds":
+    case "cloud":
+    case "smoke":
+    case "mist":
+    case "haze":
+    case "hurricane":
+    case "tornado":
+      src = "./cloud.png";
+      break;
+    case "light rain":
+      src = "./rain-sun.png";
+      break;
+    case "moderate rain":
+    case "heavy rain":
+    case "intense rain":
+      src = "./rain.png";
+      break;
+    case "light shower rain":
+    case "moderate shower rain":
+    case "heavy shower rain":
+      src = "./shower-rain.png";
+      break;
+    case "light snow":
+    case "moderate snow":
+    case "heavy snow":
+      src = "./winter.png";
+      break;
+    case "thunderstorm":
+    case "heavy thunderstorm":
+      src = "./thunder.png";
+      break;
+    case "rain and thunder":
+      src = "./rain-thunder.png";
+      break;
+    case "snow and thunder":
+      src = "./snow-thunder.png";
+      break;
+    case "sandstorm":
+    case "duststorm":
+      src = "./tormenta.png";
+      break;
+    default:
+      src = "./cloud.png";
+      break;
+  }
 
-    const handleImageLoad = () => {
-        setLoading(false);
-    };
+  const handleImageLoad = () => {
+    setLoading(false);
+  };
 
-    return (
-        <div className={`relative ${divClassName} w-full z-[9]`}>
-            {loading && <div className="loader relative top-[10rem]" />}
-            <img
-                key={src}
-                src={src}
-                alt={weather}
-                className={`pulse absolute w-full object-contain ${imgClassName} ${
-                    loading ? 'hidden' : 'block'
-                }`}
-                onLoad={handleImageLoad}
-            />
-        </div>
-    );
+  return (
+    <div className={`relative ${divClassName} w-full z-[9]`}>
+      {loading && <div className=" relative top-[10rem]" />}
+      <img
+        key={src}
+        src={src}
+        alt={weather}
+        className={`  w-full object-contain ${imgClassName} ${
+          // absolute
+          loading ? "hidden" : "block"
+        }`}
+        onLoad={handleImageLoad}
+      />
+    </div>
+  );
 };
 
 export default ImageLoader;
