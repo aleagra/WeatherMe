54415d6de28562de8c3d881260b0f3742e6bd2fc
Lucas Samuel Pereyra <lucaspereyra470@gmail.com>
2023-04-12 16:03:43 -0300

    cards updated

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,126 +1,143 @@
-import ImageLoader from "./Home/ImageLoader";
-import { Splide, SplideSlide, SplideTrack } from "@splidejs/react-splide";
-import "@splidejs/react-splide/css";
+import { useState } from 'react';
+import ImageLoader from './Home/ImageLoader';
+import { Splide, SplideSlide, SplideTrack } from '@splidejs/react-splide';
+import '@splidejs/react-splide/css';
 
 // or other themes
-import "@splidejs/react-splide/css/skyblue";
-import "@splidejs/react-splide/css/sea-green";
+import '@splidejs/react-splide/css/skyblue';
+import '@splidejs/react-splide/css/sea-green';
 
 // or only core styles
-import "@splidejs/react-splide/css/core";
+import '@splidejs/react-splide/css/core';
 
 const Cards = ({ data, dates }) => {
-  return (
-    <>
-      <Splide
-        hasTrack={false}
-        options={{
-          perPage: 4,
-          rewind: false,
-          perMove: 1,
-          gap: "3rem",
-          padding: "1.5rem",
-          pagination: false,
-          // breakpoints:{
-          //     1440:{
-          //         perPage:3,
-          //         gap:"3rem",
-          //     },
-          //     640:{
-          //         perPage:1
-          //     }
-          // }
-        }}
-      >
-        <SplideTrack>
-          {dates.map((date, index) => {
-            const items = data[date].map((item) => item); // Guarda los items en un nuevo array
-            const itemsZero = items[0];
-            console.log(items);
-            return (
-              <SplideSlide>
-                <div
-                  key={index}
-                  className=" px-[1rem] h-[30rem]  py-4 rounded-2xl  bg-gradient-to-tr  from-blue-500/70 to-blue-600/80   shadow-lg  text-white hover:bg-white/10 cursor-pointer"
-                >
-                  <div className="flex gap-1 flex-col text-center h-full w-full">
-                    <span>{date}</span>
-                    <ImageLoader
-                      data={itemsZero}
-                      divClassName="mt-6"
-                      imgClassName="w-full h-[16rem]"
-                    />
-                    <div className="flex flex-col mt-auto mx-auto">
-                      <span className="mb-6 text-5xl font-bold">
-                        {parseInt(itemsZero.main.temp)} °C
-                      </span>
-                      <div className="flex gap-x-4">
-                        <div className="flex flex-col gap-y-1">
-                          <span className="text-sm opacity-80">Humidity</span>
-                          <span className="text-lg font-bold">
-                            {itemsZero.main.humidity}{" "}
-                            <span className="text-sm font-light">%</span>
-                          </span>
-                        </div>
-                        <div className="flex flex-col gap-y-1">
-                          <span className="text-sm opacity-80">Feels like</span>
-                          <span className="text-lg font-bold">
-                            {parseInt(itemsZero.main.feels_like)}{" "}
-                            <span className="text-sm font-light">°C</span>
-                          </span>
-                        </div>
-                        <div className="flex flex-col gap-y-1">
-                          <span className="text-sm opacity-80">Wind</span>
-                          <span className="text-lg font-bold">
-                            {itemsZero.wind.speed}{" "}
-                            <span className="text-sm font-light">km/h</span>
-                          </span>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
+    const [active, setActive] = useState(false);
+    const [details, setDetails] = useState([]);
+
+    return (
+        <>
+            <Splide
+                hasTrack={false}
+                options={{
+                    perPage: 4,
+                    rewind: false,
+                    perMove: 1,
+                    gap: '3rem',
+                    padding: '1.5rem',
+                    pagination: false,
+                }}
+            >
+                <SplideTrack>
+                    {dates.map((date, index) => {
+                        const items = data[date].map((item) => item); // Guarda los items en un nuevo array
+                        const itemsZero = items[0];
+                        return (
+                            <SplideSlide key={index}>
+                                <div
+                                    className="px-[1rem] h-[25rem] py-4 rounded-2xl bg-blue-500 shadow-md  text-white hover:bg-blue-700/90 transition-colors cursor-pointer"
+                                    onClick={() => {
+                                        setActive(true);
+                                        setDetails(items);
+                                    }}
+                                >
+                                    <div className="flex gap-1 flex-col text-center h-full w-full">
+                                        <span>{date}</span>
+                                        <ImageLoader
+                                            data={itemsZero}
+                                            divClassName="mt-6"
+                                            imgClassName="w-full h-[11rem]"
+                                        />
+                                        <div className="flex flex-col mt-auto mx-auto">
+                                            <span className="mb-6 text-5xl font-bold">
+                                                {parseInt(itemsZero.main.temp)} °C
+                                            </span>
+                                            <div className="flex gap-x-4">
+                                                <div className="flex flex-col gap-y-1">
+                                                    <span className="text-sm opacity-80">
+                                                        Humidity
+                                                    </span>
+                                                    <span className="text-lg font-bold">
+                                                        {itemsZero.main.humidity}{' '}
+                                                        <span className="text-sm font-light">
+                                                            %
+                                                        </span>
+                                                    </span>
+                                                </div>
+                                                <div className="flex flex-col gap-y-1">
+                                                    <span className="text-sm opacity-80">
+                                                        Feels like
+                                                    </span>
+                                                    <span className="text-lg font-bold">
+                                                        {parseInt(itemsZero.main.feels_like)}{' '}
+                                                        <span className="text-sm font-light">
+                                                            °C
+                                                        </span>
+                                                    </span>
+                                                </div>
+                                                <div className="flex flex-col gap-y-1">
+                                                    <span className="text-sm opacity-80">Wind</span>
+                                                    <span className="text-lg font-bold">
+                                                        {itemsZero.wind.speed}{' '}
+                                                        <span className="text-sm font-light">
+                                                            km/h
+                                                        </span>
+                                                    </span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </SplideSlide>
+                        );
+                    })}
+                </SplideTrack>
+                <div className="splide__arrows">
+                    <button className="splide__arrow splide__arrow--prev">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="w-6 h-6 stroke-blue-700"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M8.25 4.5l7.5 7.5-7.5 7.5"
+                            />
+                        </svg>
+                    </button>
+                    <button className="splide__arrow splide__arrow--next">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="w-6 h-6 stroke-blue-700"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M8.25 4.5l7.5 7.5-7.5 7.5"
+                            />
+                        </svg>
+                    </button>
                 </div>
-              </SplideSlide>
-            );
-          })}
-        </SplideTrack>
-        <div className="splide__arrows">
-          <button className="splide__arrow splide__arrow--prev">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6 stroke-white"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M8.25 4.5l7.5 7.5-7.5 7.5"
-              />
-            </svg>
-          </button>
-          <button className="splide__arrow splide__arrow--next">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6 stroke-white"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M8.25 4.5l7.5 7.5-7.5 7.5"
-              />
-            </svg>
-          </button>
-        </div>
-      </Splide>
-    </>
-  );
+            </Splide>
+
+            {active && (
+                <div className="px-[5rem]">
+                    <div className="w-full h-fit bg-black text-white">
+                        {details.map((item) => (
+                            <p>{item.main.temp}</p>
+                        ))}
+                    </div>
+                </div>
+            )}
+        </>
+    );
 };
 
 export default Cards;
--- src/components/Home/WeekWeather.jsx
+++ src/components/Home/WeekWeather.jsx
@@ -1,61 +1,60 @@
-import React, { useEffect, useState } from "react";
-import { useContext } from "react";
-import { WeatherContext } from "../context/WeatherContext";
-import Cards from "../Cards";
-import Navbar from "../Navbar/Navbar";
+import React, { useEffect, useState } from 'react';
+import { useContext } from 'react';
+import { WeatherContext } from '../context/WeatherContext';
+import Cards from '../Cards';
+import Navbar from '../Navbar/Navbar';
 
 function WeekWeather() {
-  const { city, setLoading, loading } = useContext(WeatherContext);
-  const [forecast, setForecast] = useState([]);
-  const [dates, setDates] = useState([]);
-  const [dataLoaded, setDataLoaded] = useState(false);
+    const { city, setLoading, loading } = useContext(WeatherContext);
+    const [forecast, setForecast] = useState([]);
+    const [dates, setDates] = useState([]);
+    const [dataLoaded, setDataLoaded] = useState(false);
 
-  const fetchData = () => {
-    fetch(
-      `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=9175b446bb90dac9af29b18ba0c06899&units=metric`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        const forecastsByDay = data.list
-          .slice(0, 40) // solo tomamos los primeros 40 resultados (5 días a 8 pronósticos por día)
-          .reduce((forecasts, forecast) => {
-            const date = forecast.dt_txt.split(" ")[0]; // obtener la fecha sin la hora
+    const fetchData = () => {
+        fetch(
+            `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=9175b446bb90dac9af29b18ba0c06899&units=metric`
+        )
+            .then((response) => response.json())
+            .then((data) => {
+                const forecastsByDay = data.list
+                    .slice(0, 40) // solo tomamos los primeros 40 resultados (5 días a 8 pronósticos por día)
+                    .reduce((forecasts, forecast) => {
+                        const date = forecast.dt_txt.split(' ')[0]; // obtener la fecha sin la hora
 
-            if (!forecasts[date]) {
-              forecasts[date] = []; // si es la primera vez que se encuentra la fecha, se crea un nuevo array vacío
-            }
-            forecasts[date].push(forecast); // se agrega el pronóstico al array correspondiente al día
+                        if (!forecasts[date]) {
+                            forecasts[date] = []; // si es la primera vez que se encuentra la fecha, se crea un nuevo array vacío
+                        }
+                        forecasts[date].push(forecast); // se agrega el pronóstico al array correspondiente al día
 
-            return forecasts;
-          }, {});
+                        return forecasts;
+                    }, {});
 
-        const allDates = Object.keys(forecastsByDay); // obtenemos un array con todas las fechas
+                const allDates = Object.keys(forecastsByDay); // obtenemos un array con todas las fechas
 
-        setDates(allDates); // actualizamos el array con todas las fechas
-        setForecast([forecastsByDay]); // actualizamos los pronósticos por día
-      })
-      .catch((error) => console.error(error));
-  };
+                setDates(allDates); // actualizamos el array con todas las fechas
+                setForecast([forecastsByDay]); // actualizamos los pronósticos por día
+            })
+            .catch((error) => console.error(error));
+    };
 
-  useEffect(() => {
-    fetchData();
-  }, [city]);
+    useEffect(() => {
+        fetchData();
+    }, [city]);
 
-  if (loading) {
-    return <div className="loader bg-primary" />;
-  }
+    if (loading) {
+        return <div className="loader bg-primary" />;
+    }
 
-  return (
-    <>
-      <Navbar color=" " />
-      <section className="w-full h-full  flex justify-center  px-[3rem] ">
-        <div className="h-fit ">
-          {forecast &&
-            forecast.map((item) => <Cards data={item} dates={dates} />)}
-        </div>
-      </section>
-    </>
-  );
+    return (
+        <>
+            <Navbar color=" " />
+            <section className="w-full h-full flex flex-col items-center px-[3rem]">
+                <div className="h-fit py-2">
+                    {forecast && forecast.map((item) => <Cards data={item} dates={dates} />)}
+                </div>
+            </section>
+        </>
+    );
 }
 
 export default WeekWeather;
