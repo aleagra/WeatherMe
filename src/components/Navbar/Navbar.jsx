05f78af51d263677508df66c3a85c4927069250f
Lucas Samuel Pereyra <lucaspereyra470@gmail.com>
2023-04-20 15:05:29 -0300

    bugs aboutus and navbar solved

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,93 +1,90 @@
-import React, { useEffect, useState } from "react";
-import { NavLink } from "react-router-dom";
-import MapPin from "../../utilities/icons/MapPin";
-import { useContext } from "react";
-import { WeatherContext } from "../../context/WeatherContext";
-import toast, { Toaster } from "react-hot-toast";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import MapPin from '../../utilities/icons/MapPin';
+import { useContext } from 'react';
+import { WeatherContext } from '../../context/WeatherContext';
+import toast, { Toaster } from 'react-hot-toast';
+import { Link } from 'react-router-dom';
 // icons
 
 function Navbar({ color }) {
-  const { data, setData, setLoading, city, setCity } =
-    useContext(WeatherContext);
-  const [error, setError] = useState("");
-  const notify = () => toast("Here is your toast.");
-  let anchoPantalla =
-    window.innerWidth ||
-    document.documentElement.clientWidth ||
-    document.body.clientWidth;
-  const getWeather = () => {
-    setLoading(true);
-    fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=9175b446bb90dac9af29b18ba0c06899&units=metric&rain`
-    )
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("City not found");
-        }
-        return response.json();
-      })
-      .then((data) => {
-        setData(data);
-        localStorage.setItem("city", JSON.stringify(city));
-      })
-      .catch((error) => {
-        setError(error);
-        toast.error("Enter an available location.");
-      })
-      .finally(() => setLoading(false));
-  };
-
-  useEffect(() => {
-    getWeather();
-  }, [city]);
-
-  return (
-    <header className={`w-full h-fit lg:h-[6rem] ${color}`}>
-      <nav className="h-full w-full md:px-[2rem] xl:px-[4rem] 1440:px-[8rem] py-4 gap-y-6 flex flex-col md:flex-row md:justify-between text-black items-center">
-        <div className="flex items-center">
-          <Link to="/">
-            {" "}
-            <img
-              src={anchoPantalla <= 1280 ? "./whiteLogo.png" : "/logo.png"}
-              alt=""
-              className="min-w-[150px] w-[200px]"
-            />
-          </Link>
-        </div>
-        <div className="flex gap-2 xl:gap-x-[2rem] 1440:-mr-8 flex-col md:flex-row items-center 420:flex-row z-20">
-          <div className="flex">
-            <span
-              className={`py-2 px-4 whitespace-nowrap md:py-3 text-center ${
-                data ? "bg-black/70 text-white" : "bg-transparent"
-              }  rounded-2xl outline-none font-medium appearance-none`}
-            >
-              {data && (
-                <>
-                  <span>{data.name}, </span>
-                  <span>{data.sys.country}</span>
-                </>
-              )}
-            </span>
-          </div>
-          <div className="text-white relative flex">
-            <MapPin />
-            <input
-              className="w-[13rem] pl-[2.5rem] pr-2 text-white whitespace-nowrap py-3 outline-none bg-black/70 rounded-2xl placeholder:text-white"
-              type="search"
-              placeholder="Search your location"
-              onKeyDown={(event) => {
-                if (event.key === "Enter") {
-                  setCity(event.target.value);
+    const { data, setData, setLoading, city, setCity } = useContext(WeatherContext);
+    const [error, setError] = useState('');
+    const notify = () => toast('Here is your toast.');
+    let anchoPantalla =
+        window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    const getWeather = () => {
+        setLoading(true);
+        fetch(
+            `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=9175b446bb90dac9af29b18ba0c06899&units=metric&rain`
+        )
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error('City not found');
                 }
-              }}
-            />
-          </div>
-        </div>
-      </nav>
-      <Toaster position="top-center" reverseOrder={false} />
-    </header>
-  );
+                return response.json();
+            })
+            .then((data) => {
+                setData(data);
+                localStorage.setItem('city', JSON.stringify(city));
+            })
+            .catch((error) => {
+                setError(error);
+                toast.error('Enter an available location.');
+            })
+            .finally(() => setLoading(false));
+    };
+
+    useEffect(() => {
+        getWeather();
+    }, [city]);
+
+    return (
+        <header className={`h-fit w-full lg:h-[6rem] ${color}`}>
+            <nav className="flex h-full w-full flex-col items-center gap-y-6 py-4 text-black md:flex-row md:justify-between md:px-[2rem] xl:px-[4rem] 1440:px-[8rem]">
+                <div className="flex items-center">
+                    <Link to="/">
+                        {' '}
+                        <img
+                            src={anchoPantalla <= 1280 ? './whiteLogo.png' : '/logo.png'}
+                            alt="logo"
+                            className="w-[200px] min-w-[150px]"
+                        />
+                    </Link>
+                </div>
+                <div className="420:flex-row z-20 flex flex-col items-center gap-2 md:flex-row xl:gap-x-[2rem] 1440:-mr-8">
+                    <div className="flex">
+                        <span
+                            className={`whitespace-nowrap py-2 px-4 text-center md:py-3 ${
+                                data ? 'bg-black/70 text-white' : 'bg-transparent'
+                            }  appearance-none rounded-2xl font-medium outline-none`}
+                        >
+                            {data && (
+                                <>
+                                    <span>{data.name}, </span>
+                                    <span>{data.sys.country}</span>
+                                </>
+                            )}
+                        </span>
+                    </div>
+                    <div className="relative flex text-white">
+                        <MapPin />
+                        <input
+                            className="w-[13rem] whitespace-nowrap rounded-2xl bg-black/70 py-3 pl-[2.5rem] pr-2 text-white outline-none placeholder:text-white"
+                            type="search"
+                            placeholder="Search your location"
+                            onKeyDown={(event) => {
+                                if (event.key === 'Enter') {
+                                    setCity(event.target.value);
+                                }
+                            }}
+                        />
+                    </div>
+                </div>
+            </nav>
+            <Toaster position="top-center" reverseOrder={false} />
+        </header>
+    );
 }
 
 export default Navbar;
--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -1,108 +1,103 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import {
-  LinkedinIcon,
-  GithubIcon,
-  WebsiteIcon,
-  PeopleIcon,
-  BackIcon,
-} from "../../utilities";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { LinkedinIcon, GithubIcon, WebsiteIcon, PeopleIcon, BackIcon } from '../../utilities';
 
 const AboutUs = () => {
-  return (
-    <div className="overflow-hidden h-screen max-sm:overflow-auto">
-      <header className="w-full">
-        <div className="flex gap-x-[4rem] justify-center items-center mt-[4rem]">
-          <Link to="/">
-            {" "}
-            <img src="./logo.png" alt="logo" className="w-[300px]" />
-          </Link>
-        </div>
-      </header>
+    return (
+        <div className="overflow-hidden max-sm:overflow-auto lg:h-screen">
+            <header className="relative w-full">
+                <div className="mt-[4rem] flex items-center justify-center gap-x-[4rem]">
+                    <Link to="/">
+                        {' '}
+                        <img src="./logo.png" alt="logo" className="w-[300px]" />
+                    </Link>
+                </div>
+            </header>
 
-      <main className="flex w-full flex-col h-full justify-center items-center max-sm:mt-[6rem]">
-        <h1 className="font-bold text-2xl mb-[2rem]">Our Team</h1>
-        <div className="max-sm:mb-[4rem] flex gap-8 max-sm:flex-col">
-          <div className="h-[26rem] w-[18rem] min-w-[15rem] bg-gray-100 rounded-lg flex flex-col gap-4 hover:shadow-lg transition-all hover:-translate-y-2">
-            <img
-              src="./ale.jpg"
-              alt="caripela"
-              className="w-full h-[65%] object-cover"
-            />
-            <div className="flex flex-col text-center">
-              <span className="font-bold text-xl">Alejandro Agra</span>
-              <span className="opacity-60 text-sm">Full Stack Developer</span>
-            </div>
-            <div className="flex gap-6 justify-center mt-auto pb-4">
-              <a
-                href="https://www.linkedin.com/in/alejandro-agra/"
-                target="_blank"
-                className="rounded-full bg-slate-200 p-2 hover:bg-blue-400 transition-colors"
-              >
-                <LinkedinIcon />
-              </a>
-              <a
-                href="https://github.com/aleagra"
-                target="_blank"
-                className="rounded-full bg-slate-200 p-2 hover:bg-blue-400 transition-colors"
-              >
-                <GithubIcon />
-              </a>
-              <a
-                href="#"
-                target="_blank"
-                className="rounded-full bg-slate-200 p-2 hover:bg-blue-400 transition-colors"
-              >
-                <WebsiteIcon />
-              </a>
-            </div>
-          </div>
+            <main className="mt-[3rem] flex h-full w-full flex-col items-center justify-center">
+                <h1 className="mb-[2rem] text-2xl font-bold">Our Team</h1>
+                <div className="flex gap-8 max-sm:mb-[4rem] max-sm:flex-col">
+                    <div className="flex h-[26rem] w-[18rem] min-w-[15rem] flex-col gap-4 rounded-lg bg-gray-100 transition-all hover:-translate-y-2 hover:shadow-lg">
+                        <img
+                            src="./ale.jpg"
+                            alt="caripela"
+                            className="h-[65%] w-full object-cover"
+                        />
+                        <div className="flex flex-col text-center">
+                            <span className="text-xl font-bold">Alejandro Agra</span>
+                            <span className="text-sm opacity-60">Full Stack Developer</span>
+                        </div>
+                        <div className="mt-auto flex justify-center gap-6 pb-4">
+                            <a
+                                href="https://www.linkedin.com/in/alejandro-agra/"
+                                target="_blank"
+                                className="rounded-full bg-slate-200 p-2 transition-colors hover:bg-blue-400"
+                            >
+                                <LinkedinIcon />
+                            </a>
+                            <a
+                                href="https://github.com/aleagra"
+                                target="_blank"
+                                className="rounded-full bg-slate-200 p-2 transition-colors hover:bg-blue-400"
+                            >
+                                <GithubIcon />
+                            </a>
+                            <a
+                                href="#"
+                                target="_blank"
+                                className="rounded-full bg-slate-200 p-2 transition-colors hover:bg-blue-400"
+                            >
+                                <WebsiteIcon />
+                            </a>
+                        </div>
+                    </div>
 
-          <div className="h-[26rem] w-[18rem] min-w-[15rem] bg-gray-100 rounded-lg flex flex-col gap-4 hover:shadow-lg transition-all hover:-translate-y-2">
-            <img
-              src="./lucas.jpeg"
-              alt="caripela"
-              className="w-full h-[65%] object-cover"
-            />
-            <div className="flex flex-col text-center">
-              <span className="font-bold text-xl">Lucas Pereyra</span>
-              <span className="opacity-60 text-sm">Frontend Developer</span>
-            </div>
-            <div className="flex gap-6 justify-center mt-auto pb-4">
-              <a
-                href="https://www.linkedin.com/in/lucaspereyradev/"
-                target="_blank"
-                className="rounded-full bg-slate-200 p-2 hover:bg-blue-400 transition-colors"
-              >
-                <LinkedinIcon />
-              </a>
-              <a
-                href="https://github.com/lucaspereyradev"
-                target="_blank"
-                className="rounded-full bg-slate-200 p-2 hover:bg-blue-400 transition-colors"
-              >
-                <GithubIcon />
-              </a>
-              <a
-                href="https://lucaspereyradev.github.io/portfolio/"
-                target="_blank"
-                className="rounded-full bg-slate-200 p-2 hover:bg-blue-400 transition-colors"
-              >
-                <WebsiteIcon />
-              </a>
-            </div>
-          </div>
+                    <div className="flex h-[26rem] w-[18rem] min-w-[15rem] flex-col gap-4 rounded-lg bg-gray-100 transition-all hover:-translate-y-2 hover:shadow-lg">
+                        <img
+                            src="./lucas.jpeg"
+                            alt="caripela"
+                            className="h-[65%] w-full object-cover"
+                        />
+                        <div className="flex flex-col text-center">
+                            <span className="text-xl font-bold">Lucas Pereyra</span>
+                            <span className="text-sm opacity-60">Frontend Developer</span>
+                        </div>
+                        <div className="mt-auto flex justify-center gap-6 pb-4">
+                            <a
+                                href="https://www.linkedin.com/in/lucaspereyradev/"
+                                target="_blank"
+                                className="rounded-full bg-slate-200 p-2 transition-colors hover:bg-blue-400"
+                            >
+                                <LinkedinIcon />
+                            </a>
+                            <a
+                                href="https://github.com/lucaspereyradev"
+                                target="_blank"
+                                className="rounded-full bg-slate-200 p-2 transition-colors hover:bg-blue-400"
+                            >
+                                <GithubIcon />
+                            </a>
+                            <a
+                                href="https://lucaspereyradev.github.io/portfolio/"
+                                target="_blank"
+                                className="rounded-full bg-slate-200 p-2 transition-colors hover:bg-blue-400"
+                            >
+                                <WebsiteIcon />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="mb-8 flex items-center gap-4 px-2  md:justify-center xl:my-12 xl:justify-start">
+                    <Link to="/">
+                        <button className="btn flex items-center gap-2 bg-blue-500 text-white/95">
+                            <BackIcon /> Back to Home
+                        </button>
+                    </Link>
+                </div>
+            </main>
         </div>
-        <div className="flex gap-4  items-center px-2  md:mb-8  md:justify-center xl:justify-start xl:my-12">
-          <Link to="/">
-            <button className="btn flex gap-2 items-center bg-blue-500 text-white/95">
-              <BackIcon /> Back to Home
-            </button>
-          </Link>
-        </div>
-      </main>
-    </div>
-  );
+    );
 };
 
 export default AboutUs;
