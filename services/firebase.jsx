bd1d4d32b61b5d324c0871cc33f4d2124a93be20
Lucas Samuel Pereyra <lucaspereyra470@gmail.com>
2023-04-20 22:13:08 -0300

    firebase hosting

---
+++ services/firebase.jsx
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyButQg2acGcgRKxwboSO7noCwjsM5g4yLw',
+    authDomain: 'weatherme-bc54b.firebaseapp.com',
+    projectId: 'weatherme-bc54b',
+    storageBucket: 'weatherme-bc54b.appspot.com',
+    messagingSenderId: '762768622374',
+    appId: '1:762768622374:web:e3e8d30be17f5b4245f7e7',
+};
+
+const app = initializeApp(firebaseConfig);
