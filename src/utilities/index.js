8cc65a6d5e81c45474d2249e55813e6f6007e886
Alejandro Agra <aleagra08@hotmail.com>
2023-04-21 11:50:26 -0300

    fixed bug carrousel

--- src/utilities/index.js
+++ src/utilities/index.js
@@ -1,6 +1,7 @@
 export { default as BackIcon } from './icons/BackIcon';
 export { default as DropletIcon } from './icons/DropletIcon';
 export { default as GithubIcon } from './icons/GithubIcon';
+export { default as LeftArrowIcon } from './icons/LeftArrowIcon';
 export { default as LinkedinIcon } from './icons/LinkedinIcon';
 export { default as MapPin } from './icons/MapPin';
 export { default as PeopleIcon } from './icons/PeopleIcon';
@@ -8,4 +9,3 @@ export { default as SunIcon } from './icons/SunIcon';
 export { default as WebsiteIcon } from './icons/WebsiteIcon';
 export { default as WindIcon } from './icons/WindIcon';
 export { default as WorldIcon } from './icons/WorldIcon';
-export * from './index';
